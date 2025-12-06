82a94afd267f4f1014c4ac376331713cd9badb3f
Sukhtaj Dhillon <you@example.com>
2025-12-05 18:10:53 -0600

    checked if the limiter was working or not

--- src/api/v1/middleware/express-rate-limit.ts
+++ src/api/v1/middleware/express-rate-limit.ts
@@ -6,7 +6,7 @@ import rateLimit from "express-rate-limit";
  */
 export const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 100,
+    max: 1,
     standardHeaders: true,
     legacyHeaders: false
 });
